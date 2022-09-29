d6cbae90103d4f535bd32934f8a703c8e673e13a
Emílio Biasi <ejbiasi@hotmail.com>
2022-09-29 20:30:55 -0300

    deixando somente o front_web aqui

--- backend/src/main/java/com/app/backend/repository/ClientRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.backend.repository;
-
-import com.app.backend.model.Client;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ClientRepository  extends MongoRepository<Client, String> {
-}
--- backend/src/main/java/com/app/backend/repository/DocumentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.backend.repository;
-
-import com.app.backend.model.Client;
-import com.app.backend.model.Document;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface DocumentRepository extends MongoRepository<Document, String> {
-}
