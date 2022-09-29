9241eb78ee6cecfef1b5c710c6cf7aed255271fd
Elias-FS <elias-fausto@hotmail.com>
2022-09-29 02:34:46 -0300

    Atualizando Front-End

--- site/via_facil_web/src/components/heroSection/index.js
+++ site/via_facil_web/src/components/heroSection/index.js
@@ -1,16 +1,23 @@
-import React from 'react'
-import { Button } from '../button'
-import './HeroSection.css'
+import React from "react";
+import { Button } from "../button";
+import "./HeroSection.css";
 
 const HeroSection = () => {
   return (
-    <div className='heroSection'>
-        <h1>Via <span>Fácil</span></h1>
-        <p>Faça a emissão da sua segunda via com maior facilidade e segurança com a VIA FÁCIL
-Sem mais burocrácias e demoras</p>
+    <>
+      <video src="/videos/video-2.mp4" autoPlay loop muted />
+      <div className="heroSection">
+        <h1>
+          Via <span>Fácil</span>
+        </h1>
+        <p>
+          Faça a emissão da sua segunda via com maior facilidade e segurança com
+          a VIA FÁCIL Sem mais burocrácias e demoras
+        </p>
         <Button>Comece já</Button>
-    </div>
-  )
-}
+      </div>
+    </>
+  );
+};
 
-export default HeroSection
+export default HeroSection;
