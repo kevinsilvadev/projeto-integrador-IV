81d795b963b04b2874fdc1ed33c94f319d578111
Emílio Biasi <ejbiasi@hotmail.com>
2022-09-29 20:33:06 -0300

    deixando somente o front_mobile aqui

--- backend/src/main/java/com/app/backend/repository/CompanyRepository.java
+++
@@ -1,8 +0,0 @@
-package com.app.backend.repository;
-
-import com.app.backend.model.Client;
-import com.app.backend.model.Company;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface CompanyRepository extends MongoRepository<Company, String> {
-}
--- backend/src/main/java/com/app/backend/services/exception/ObjectNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.app.backend.services.exception;
-
-public class ObjectNotFoundException extends  RuntimeException {
-    public ObjectNotFoundException(String msg) {
-        super(msg);
-    }
-}
