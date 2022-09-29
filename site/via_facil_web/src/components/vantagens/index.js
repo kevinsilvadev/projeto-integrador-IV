1d4d0e74bdae19a7aa5541fe3e98d12d0b23e3b6
Emílio Biasi <ejbiasi@hotmail.com>
2022-09-29 14:14:17 -0300

    teste 971

--- site/via_facil_web/src/components/vantagens/index.js
+++ site/via_facil_web/src/components/vantagens/index.js
@@ -7,7 +7,7 @@ const Vantagens = () => {
       <h1 className="title">Vantagens</h1>
       <div className="vantagens-container">
         <Card
-          title="Card Title"
+          title="Facilidade"
           imageUrl="https://img.elo7.com.br/product/original/3249FFD/quadro-decorativo-natureza-paisagem-mar-sol-coqueiro-ponte-quadro-belezas-naturais.jpg"
           body="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut. Lorem ipsum dolor sit amet.!"
         />
