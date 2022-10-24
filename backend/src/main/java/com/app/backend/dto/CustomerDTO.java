b022a44d1e78d3f358e3674c5fa08989ac490b47
kevinsilvadev <kivim2018@gmail.com>
2022-10-24 20:56:07 -0300

    Atualiando

---
+++ backend/src/main/java/com/app/backend/dto/CustomerDTO.java
@@ -0,0 +1,34 @@
+package com.app.backend.dto;
+
+import com.app.backend.model.Customer;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.io.Serializable;
+
+@Document
+@Getter
+@Setter
+@NoArgsConstructor
+public class CustomerDTO implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    private String id;
+
+    private String cpf;
+    private String name;
+    private String email;
+    private String senha;
+    public CustomerDTO(Customer obj) {
+        id = obj.getId();
+        cpf = obj.getCpf();
+        name = obj.getName();
+        email = obj.getEmail();
+        senha = obj.getSenha();
+    }
+}
